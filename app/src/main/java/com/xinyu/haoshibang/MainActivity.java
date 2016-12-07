f8bb062485ca8dfa2bcf503ad46f7df73a4a2c5e
dzf <1067210371@qq.com>
2016-12-07 15:56:07 +0800

    显示

--- app/src/main/java/com/xinyu/haoshibang/MainActivity.java
+++ app/src/main/java/com/xinyu/haoshibang/MainActivity.java
@@ -1,4 +1,7 @@
 package com.xinyu.haoshibang;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -6,4 +9,9 @@ import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("显示!");
+    }
 }
